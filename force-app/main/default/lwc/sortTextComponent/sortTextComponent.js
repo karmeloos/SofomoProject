194e1b2742fcd24db600dc235b1818dc4227c85d
Jakub Fik <fikkuba@gmail.com>
2023-09-05 13:51:43 +0200

    Excercise Container init

--- force-app/main/default/lwc/sortTextComponent/sortTextComponent.js
+++ force-app/main/default/lwc/sortTextComponent/sortTextComponent.js
@@ -1,18 +1,24 @@
-import { LightningElement } from 'lwc';
-import {SortJavaScriptController} from './sortJavaScriptController.js';
+import { LightningElement, api } from "lwc";
+import { SortJavaScriptController } from "./sortJavaScriptController.js";
+
+const CONTROLLERS_MAP = new Map()
+CONTROLLERS_MAP.set('JavaScript', new SortJavaScriptController());
 
 export default class SortTextComponent extends LightningElement {
-    text;
-    cardTitle = 'Sort text by JS';
-    controller;
+  text;
+  @api cardTitle;
+  @api controllerType;
+  controller;
 
-    connectedCallback() {
-        this.controller = new SortJavaScriptController();
-        this.text = this.controller.getDefaultText();
-    }
+  connectedCallback() {
+    console.log(this.controllerType);
+    if(! CONTROLLERS_MAP.has(this.controllerType)) return;
+    this.controller = CONTROLLERS_MAP.get(this.controllerType);
+    this.text = this.controller.getDefaultText();
+  }
 
-    handleSort() {
-        const currentTextValue = this.refs.textArea.value;
-        this.text = this.controller.sortText(currentTextValue);
-    }
+  handleSort() {
+    const currentTextValue = this.refs.textArea.value;
+    this.text = this.controller.sortText(currentTextValue);
+  }
 }
